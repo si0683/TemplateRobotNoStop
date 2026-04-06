a1f33f3b2bcb0e9cb6c4fd8918327132ad1dc238
Vasiliy Sidorenko <vasiliysidorenko63@yandex.ru>
2026-04-06 10:23:51 +0400

    Добавьте файлы проекта.

---
+++ TemplateRobotNoStop.cs
@@ -0,0 +1,22 @@
+﻿using OsEngine.Entity;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Robots
+{
+    [Bot("TemplateRobotNoStop")]
+    public class TemplateRobotNoStop : BotPanel
+    {
+        public TemplateRobotNoStop(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+
+        }
+    }
+
+}
+
